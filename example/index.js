5b2d1e5d2e92dfe78fd4533405650e5beeca0407
Ionică Bizău <bizauionica@gmail.com>
2016-06-06 16:50:07 +0300

    Example

--- example/index.js
+++ example/index.js
@@ -1,5 +1,25 @@
 "use strict";
 
-const rainbowSort = require("../lib");
+const rainbowSort = require("../lib")
+    , couleurs = require("couleurs")
+    , color = require("color")
+    ;
 
-console.log(rainbowSort());
+let before = ["blue", "green", "yellow", "red", "orange"]
+  , after = rainbowSort(before)
+    // => ["red", "orange", "yellow", "green", "blue"]
+
+    // And let's print them in the output
+  , print = (arr, msg) => {
+        console.log(msg);
+        arr.forEach(c => {
+            process.stdout.write(
+                couleurs(c).fg(color(c).values.rgb) + " "
+            );
+        });
+        console.log("");
+    }
+  ;
+
+print(before, "Before");
+print(after, "After");
