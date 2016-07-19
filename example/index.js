7bef00e7c9bc95175bbbc4aa79eb44b089f74436
Ionică Bizău <bizauionica@gmail.com>
2016-07-19 10:01:19 +0300

    Playing with different algorithms

--- example/index.js
+++ example/index.js
@@ -5,7 +5,7 @@ const rainbowSort = require("../lib")
     , color = require("color")
     ;
 
-let before = ["blue", "green", "yellow", "red", "orange"]
+let before = ["#701516", "#fcd7de", "#6866fb"]
   , after = rainbowSort(before)
     // => ["red", "orange", "yellow", "green", "blue"]
 
